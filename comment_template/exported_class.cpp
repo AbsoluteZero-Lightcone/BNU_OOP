4d6e83cca4172e3791740547ffc33c0d38dc3c75
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-04-20 16:46:51 +0800

    comment template verified

--- comment_template/exported_class.cpp
+++ comment_template/exported_class.cpp
@@ -14,11 +14,49 @@
 exported_class::exported_class() {}
 exported_class::exported_class(const exported_class& source) {}
 exported_class::~exported_class() {}
-void exported_class::operator=(const exported_class& source) {}
+
 datatype1 exported_class::get_dataname1() { return m_dataname1; }
 void exported_class::set_dataname1(datatype1 t_dataname1) { m_dataname1 = t_dataname1; }
 datatype2 exported_class::get_dataname2() { return m_dataname2; }
 void exported_class::set_dataname2(datatype2 t_dataname2) { m_dataname2 = t_dataname2; }
+
+/**
+  * @brief 类内重载通过对象赋值的赋值运算符
+  * @param const exported_class& source : 待赋的值
+  * @retval 无
+  */
+void exported_class::operator=(const exported_class& source) {
+	// todo
+}
+
+/**
+  * @brief 类内重载使用整数赋值的赋值运算符
+  * @param int n : 待赋的值
+  * @retval 无
+  */
+void exported_class::operator=(int n) {
+	// todo
+}
+
+/**
+  * @brief 类内重载+=运算符
+  * @param n : 待加的值
+  * @retval 返回自身引用实现链式编程
+  */
+exported_class& exported_class::operator+=(int n) {
+	// todo
+	return *this;
+}
+
+/**
+  * @brief 类内重载自增运算符
+  * @param 无
+  * @retval 返回自身引用实现链式编程
+  */
+exported_class& exported_class::operator++() {
+	// todo
+	return *this;
+}
 
 /**
   * @brief 重载标准输出流 <<运算符
@@ -27,19 +65,64 @@ void exported_class::set_dataname2(datatype2 t_dataname2) { m_dataname2 = t_data
   * @retval ostream&
   */
 ostream& operator<<(ostream& out, const exported_class& source) {
-
+	// todo
 	return out;
 }
+
 /**
   * @brief 重载大于运算符
   * @param const exported_class& n1 : 运算符左边的值
   * @param const exported_class& n2 : 运算符右边的值
   * @retval bool, true for n1 > n2
   */
-bool operator>(const exported_class& n1, const exported_class& n2){}
-bool operator==(const exported_class& n1, const exported_class& n2){}
-exported_class operator-(exported_class n){}
-exported_class operator+(const exported_class& n1, const exported_class& n2){}
-exported_class operator-(const exported_class& n1, const exported_class& n2){}
+bool operator>(const exported_class& n1, const exported_class& n2) {
+	// todo
+	return false;
+}
+
+/**
+  * @brief 重载等于运算符
+  * @param const exported_class& n1 : 运算符左边的值
+  * @param const exported_class& n2 : 运算符右边的值
+  * @retval bool, true for n1 == n2
+  */
+bool operator==(const exported_class& n1, const exported_class& n2) {
+	// todo
+	return false;
+}
+
+/**
+  * @brief 重载负号
+  * @param exported_class n : 值传递待取反的值
+  * @retval -n
+  */
+exported_class operator-(exported_class n) {
+	// todo
+	return n;
+}
+
+/**
+  * @brief 重载加法运算符
+  * @param const exported_class& n1 : 加号左边的值
+  * @param const exported_class& n2 : 加号右边的值
+  * @retval n1 + n2
+  */
+exported_class operator+(const exported_class& n1, const exported_class& n2) {
+	exported_class temp;
+	// todo
+	return temp;
+}
+
+/**
+  * @brief 重载减法运算符
+  * @param const exported_class& n1 : 被减数
+  * @param const exported_class& n2 : 减数
+  * @retval n1 - n2
+  */
+exported_class operator-(const exported_class& n1, const exported_class& n2) {
+	exported_class temp;
+	// todo
+	return temp;
+}
 
 /********* Zhang Yifa | Absolute Zero Studio - Lightcone *******END OF FILE****/
--- comment_template/exported_class.h
+++ comment_template/exported_class.h
@@ -9,34 +9,45 @@
   ******************************************************************************
   */
 
-#ifndef __exported_class_H
-#define __exported_class_H
-
-#include <iostream>
-using namespace std;
-
-typedef int datatype1;
-typedef int datatype2;
 // 替换内容：
 // exported_class 类名
 // datatype 类型
 // dataname 变量名
 // 改文件名
 // 将宏定义改为大写
+// 删除临时使用的typedef
+typedef int datatype1;
+typedef int datatype2;
+
+
+#ifndef __exported_class_H
+#define __exported_class_H
+
+#include <iostream>
+using namespace std;
+
 
 class exported_class {
 private:
 	datatype1 m_dataname1;
 	datatype2 m_dataname2;
 public:
+	// Constructors & Deconstructor
 	exported_class();
 	exported_class(const exported_class& source);
 	~exported_class();
-	void operator=(const exported_class& source);
+
+	// Getter & Setter
 	datatype1 get_dataname1();
 	void set_dataname1(datatype1 t_dataname1);
 	datatype2 get_dataname2();
 	void set_dataname2(datatype2 t_dataname2);
+
+	// 运算符重载
+	void operator=(const exported_class& source);
+	void operator=(int n);
+	exported_class& operator+=(int n);
+	exported_class& operator++();
 };
 
 ostream& operator<<(ostream& out, const exported_class& source);
