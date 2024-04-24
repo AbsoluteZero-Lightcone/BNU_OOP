a2abb2f175e18969b5207be13a71fefb33570b6f
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-04-24 22:20:55 +0800

    work 1-1 init

---
+++ work1-1/Triangle.cpp
@@ -0,0 +1,55 @@
+/**
+  ******************************************************************************
+  * @file    Triangle.cpp
+  * @author  Zhang Yifa 202311998186
+  * @version V1.0.0
+  * @date    2024-04-24
+  * @brief   Triangle class
+  * @encode  GB2312
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "Triangle.h"
+
+/* Constructors & Deconstructor --------------------------------------------- */
+Triangle::Triangle() :
+	m_dSide1(0),
+	m_dSide2(0),
+	m_dSide3(0)
+{}
+
+Triangle::Triangle(
+	double t_dSide1,
+	double t_dSide2,
+	double t_dSide3
+) :
+	m_dSide1(t_dSide1),
+	m_dSide2(t_dSide2),
+	m_dSide3(t_dSide3)
+{}
+
+Triangle::~Triangle() {}
+
+/* Getters & Setters -------------------------------------------------------- */
+double Triangle::get_dSide1()const { return m_dSide1; }
+double Triangle::get_dSide2()const { return m_dSide2; }
+double Triangle::get_dSide3()const { return m_dSide3; }
+void Triangle::set_dSide1(double t_dSide1) { m_dSide1 = t_dSide1; }
+void Triangle::set_dSide2(double t_dSide2) { m_dSide2 = t_dSide2; }
+void Triangle::set_dSide3(double t_dSide3) { m_dSide3 = t_dSide3; }
+
+/* Exported functions ------------------------------------------------------- */
+/**
+  * @brief 重载标准输出流 <<运算符
+  * @param ostream& out : 标准输出流对象
+  * @param const Triangle& source : 待输出的对象
+  * @retval ostream&
+  */
+ostream& operator<<(ostream& out, const Triangle& source) {
+	// todo
+	return out;
+}
+
+
+/********* Zhang Yifa | Absolute Zero Studio - Lightcone *******END OF FILE****/
---
+++ work1-1/Triangle.h
@@ -0,0 +1,53 @@
+/**
+  ******************************************************************************
+  * @file    Triangle.h
+  * @author  Zhang Yifa 202311998186
+  * @version V1.0.0
+  * @date    2024-04-24
+  * @brief   Triangle class
+  * @encode  GB2312
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __Triangle_H
+#define __Triangle_H
+/* Includes ------------------------------------------------------------------*/
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+/* Class ---------------------------------------------------------------------*/
+class Triangle {
+private:
+	double m_dSide1;
+	double m_dSide2;
+	double m_dSide3;
+public:
+	// Constructors & Deconstructor
+	Triangle();
+	Triangle(
+		double t_dSide1,
+		double t_dSide2,
+		double t_dSide3
+	);
+	~Triangle();
+
+	// Getter & Setter
+	double get_dSide1()const;
+	double get_dSide2()const;
+	double get_dSide3()const;
+	void set_dSide1(double t_dSide1);
+	void set_dSide2(double t_dSide2);
+	void set_dSide3(double t_dSide3);
+	
+	friend ostream& operator<<(ostream& out, const Triangle& source);
+};
+
+/* Exported functions ------------------------------------------------------- */
+ostream& operator<<(ostream& out, const Triangle& source);
+
+#endif /* !__Triangle_H */
+
+/********* Zhang Yifa | Absolute Zero Studio - Lightcone *******END OF FILE****/
