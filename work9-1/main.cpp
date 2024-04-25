6786353d15618bd7cc64a2de6bbd02f4909bf62e
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-04-25 15:55:44 +0800

    switch

--- work9-1/main.cpp
+++ work9-1/main.cpp
@@ -17,9 +17,49 @@ using namespace std;
 #include "RMB.h"
 
 int main() {
-	RMB rmb1;
+	RMB rmb1,rmb2;
 	cout << "请构造两个人民币：";
-	cin >> rmb1;
+	cin >> rmb1>> rmb2;
+
+	while (1) {
+		char command;
+		cout << "请选择运算(A:+/S:-/Q:++i/H:i++/J:--i/K:i--/): ";
+		cin >> command;
+		switch (command){
+		case 'A':
+			[[fallthrough]];
+		case 'a':
+			cout << rmb1 << "+" << rmb2 << "=" << rmb1 + rmb2 << endl;
+			break;
+		case 'S':
+			[[fallthrough]];
+		case 's':
+
+			break;
+		case 'Q':
+			[[fallthrough]];
+		case 'q':
+
+			break;
+		case 'H':
+			[[fallthrough]];
+		case 'h':
+
+			break;
+		case 'J':
+			[[fallthrough]];
+		case 'j':
+
+			break;
+		case 'K':
+			[[fallthrough]];
+		case 'k':
+
+			break;
+		default:
+			break;
+		}
+	}
 	cout << rmb1;
 	return 0;
 }
