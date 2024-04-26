e960a6b0ab65ff26934e33068492f10cd5f5a738
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-04-27 02:47:30 +0800

    Update cleaning.bat

--- cleaning.bat
+++ cleaning.bat
@@ -1,12 +1,7 @@
-pause
-for /R %1 in (*Debug*) do (
-    if exist "%1" (
-        echo Deleting "%1"
-        del /F /Q "%1"
-    )
-for /R %1 in (*x64*) do (
-    if exist "%1" (
-        echo Deleting "%1"
-        del /F /Q "%1"
-    )
-pause
+rmdir /s /q .vs
+for /d /r %%i in (Debug) do (
+    rmdir /s /q "%%i"
+)
+for /d /r %%i in (x64) do (
+    rmdir /s /q "%%i"
+)
