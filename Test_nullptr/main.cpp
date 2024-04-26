750efa689bb53ab1f40a91eebd9e6b3f4810a916
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-04-27 01:34:53 +0800

    Resolution cleaning up

--- Test_nullptr/main.cpp
+++ Test_nullptr/main.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-	int* p = new int(1);
-	cout << NULL << (int)NULL << endl;
-	cout <<p<<" " << (int)p << endl;
+	int* p = new int[10];
+	cout << NULL << " " << (int)NULL << endl;
+	cout << p << " " << (int)p << endl;
 	delete p;
-	cout << p<<" " << (int)p << endl;
+	cout << p << " " << (int)p << endl;
 	if (!p)delete p;
-	cout << p<<" " << (int)p << endl;
+	cout << p << " " << (int)p << endl;
 	if (p)delete p;
-	cout << p<<" " << (int)p << endl;
+	cout << p << " " << (int)p << endl;
 	return 0;
 }
