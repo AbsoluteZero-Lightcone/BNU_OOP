48bc84c0abb75f1bb11c7a1500b06a50dfc7152e
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-05-13 18:23:48 +0800

    basically functioned well

--- work10-2/main.cpp
+++ work10-2/main.cpp
@@ -11,12 +11,35 @@
 
 #include <iostream>
 #include <string>
-#include <cmath>
-#include <cstdint>
+#include <fstream>
 using namespace std;
+#include "Student.h"
+
+#define LENGTH 10
 
 int main() {
-
+	Student* arr[LENGTH];
+	ifstream infile("in.txt");
+	if (!infile.is_open()) {
+		cerr << "文件打开失败" << endl;
+		return 1;
+	}
+	for (int i = 0; i < LENGTH; i++) {
+		string name, id;
+		double chinese, math, english;
+		infile >> name >> id >> chinese >> math >> english;
+		arr[i] = new Student(name, id, chinese, math, english);
+	}
+	infile.close();
+	Student::sort(arr, LENGTH);
+	cout << "排序后：" << endl;
+	cout << "姓名\t学号\t\t语文\t数学\t英语\t总分" << endl;
+	for (int i = 0; i < LENGTH; i++) {
+		cout << *arr[i] << endl;
+	}
+	for (int i = 0; i < LENGTH; i++) {
+		delete arr[i];
+	}
 	return 0;
 }
 
