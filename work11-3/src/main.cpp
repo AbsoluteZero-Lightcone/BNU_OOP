6d9d68b248cfe94e6bf83689de2d684c8a81944e
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-05-21 17:37:10 +0800

    optimize arg resolver

--- work11-3/src/main.cpp
+++ work11-3/src/main.cpp
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    main.cpp
   * @author  Zhang Yifa 202311998186
-  * @version V2.2.8
+  * @version V2.2.9
   * @date    2024-05-16
   * @brief   Entrance Function
   * @encode  GB2312
@@ -14,15 +14,68 @@ using namespace std;
 
 #include "Expression.h"
 #include "show.h"
-
+bool arg_mode_silent = true;
+const string args[] = {
+	"-s",
+	"--silent",
+	"-h",
+	"--help",
+	"-t",
+	"--test",
+	"-d",
+	"detailed",
+};
+enum ARGS {
+	ARGS_S,
+	ARGS_SILENT,
+	ARGS_H,
+	ARGS_HELP,
+	ARGS_T,
+	ARGS_TEST,
+	ARGS_D,
+	ARGS_DETAILED,
+	ARGS_COUNT,
+};
+void arg_help() {
+	cout << "Usage: eval [options] [expression]" << endl;
+	cout << "Options:" << endl;
+	cout << "  -s, --silent    Silent mode, only output the result" << endl;
+	cout << "  -h, --help      Show this help document" << endl;
+	cout << "  -t, --test      Run test cases" << endl;
+	cout << "  -d, detailed    Show detailed information" << endl;
+}
+bool arg_detector(string s) {
+	for (int i = 0; i < ARGS_COUNT; i++) {
+		if (s == args[i]) {
+			switch (i) {
+			case ARGS_H:
+			case ARGS_HELP:
+				arg_help();
+				break;
+			case ARGS_T:
+			case ARGS_TEST:
+				test(tests);
+				break;
+			case ARGS_S:
+			case ARGS_SILENT:
+				arg_mode_silent = true;
+				break;
+			case ARGS_D:
+			case ARGS_DETAILED:
+				arg_mode_silent = false;
+				break;
+			default:
+				break;
+			}
+		}
+		return true;
+	}
+	return false;
+}
 int main(int argc, char** argv) {
 	if (argc > 1) { // 接受参数，支持pipeline
-		bool silent = false;
-		for (int i = 1; i < argc; i++) {// 参数解析
-			if (string(argv[i]) == "-s" || string(argv[i]) == "--silent")
-				silent = true;// 静默模式
-		}
-		if (silent)
+		for (int i = 1; i < argc; i++)	arg_detector(argv[i]);// 参数解析
+		if (arg_mode_silent)
 			for (int i = 1; i < argc; i++) {
 				if (string(argv[i]) == "-s" || string(argv[i]) == "--silent")continue;
 				if (cmd_detector(argv[i]))continue;
@@ -41,7 +94,7 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 
-	cout << "eval 2.2.8" << endl;
+	cout << "eval 2.2.9" << endl;
 	cout << "For help, type \"help\"." << endl;
 	cout << endl;
 	do {
