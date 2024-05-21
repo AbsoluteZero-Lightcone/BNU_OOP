1b09bee8a66445387c9c58df7a4d823535aaab4b
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-05-21 18:25:52 +0800

    optimize the code structure of arg silent mode

--- work11-3/src/main.cpp
+++ work11-3/src/main.cpp
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    main.cpp
   * @author  Zhang Yifa 202311998186
-  * @version V2.4.1
+  * @version V2.4.2
   * @date    2024-05-16
   * @brief   Entrance Function
   * @encode  GB2312
@@ -18,26 +18,23 @@ using namespace std;
 int main(int argc, char** argv) {
 	if (argc > 1) { // 接受参数，支持pipeline
 		for (int i = 1; i < argc; i++)	arg_detector(argv[i]);// 参数解析
-		if (arg_mode[ARG_MODE_SILENT])
-			for (int i = 1; i < argc; i++) {
-				if (string(argv[i]) == "-s" || string(argv[i]) == "--silent")continue;
-				if (cmd_detector(argv[i]))continue;
-				try { cout << Expression::eval(argv[i]) << endl; }
-				catch (const char* err) {}
+		for (int i = 1; i < argc; i++) {
+			if (string(argv[i]) == "-s" || string(argv[i]) == "--silent")continue;
+			if (cmd_detector(argv[i]))continue;
+			try {
+				if (arg_mode[ARG_MODE_SILENT])
+					cout << Expression::eval(argv[i]) << endl;
+				else show(Expression(argv[i]));
 			}
-		else
-			for (int i = 1; i < argc; i++) {
-				if (string(argv[i]) == "-s" || string(argv[i]) == "--silent")continue;
-				if (cmd_detector(argv[i]))continue;
-				try { show(Expression(argv[i])); }
-				catch (const char* err) {
-					cerr << endl << "[Error] " << err << endl << endl;
-				}
+			catch (const char* err) {
+				cerr << endl << "[Error] " << err;
+				cerr << endl << "        For help document, use eval -h" << endl << endl;
 			}
+		}
 		return 0;
 	}
 
-	cout << "eval 2.4.1" << endl;
+	cout << "eval 2.4.2" << endl;
 	cout << "For help, type \"help\"." << endl;
 	cout << endl;
 	do {
@@ -46,14 +43,14 @@ int main(int argc, char** argv) {
 		cin >> s;
 		if (cmd_detector(s))continue;
 		try {
-			if(cli_mode[CLI_MODE_SILENT])
+			if (cli_mode[CLI_MODE_SILENT])
 				cout << Expression::eval(s) << endl;
 			else
 				show(Expression(s));
 		}
 		catch (const char* err) {
 			cerr << endl << "[Error] " << err;
-			cout << endl << "        Type 'help' or 'h' to show help document" << endl << endl;
+			cerr << endl << "        Type 'help' or 'h' to show help document" << endl << endl;
 		}
 	} while (true);
 	return 0;
