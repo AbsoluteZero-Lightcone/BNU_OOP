e08dd0c4f0f47dce85cf69dda8f647a3b6fde979
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-05-21 16:43:51 +0800

    pipeline silent mode

--- work11-3/src/main.cpp
+++ work11-3/src/main.cpp
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    main.cpp
   * @author  Zhang Yifa 202311998186
-  * @version V2.2.4
+  * @version V2.2.5
   * @date    2024-05-16
   * @brief   Entrance Function
   * @encode  GB2312
@@ -15,17 +15,31 @@ using namespace std;
 #include "Expression.h"
 #include "show.h"
 
+
 int main(int argc, char** argv) {
-	// 接受参数，支持pipeline
-	if (argc > 1) {
-		for (int i = 1; i < argc; i++) {
-			try {
-				show(Expression(argv[i]));
-			}
-			catch (const char* err) {
-				cerr << endl << "[Error] " << err << endl << endl;
-			}
+	if (argc > 1) { // 接受参数，支持pipeline
+		bool silent = false;
+		for (int i = 1; i < argc; i++) {// 参数解析
+			if (argv[i] == "-s" || argv[i] == "--silent")
+				silent = true;// 静默模式
 		}
+
+		if (silent)
+			for (int i = 1; i < argc; i++) {
+				if (argv[i] == "-s" || argv[i] == "--silent")continue;
+				if (cmd_detector(argv[i]))continue;
+				try { cout << Expression::eval(argv[i]) << endl; }
+				catch (const char* err) {}
+			}
+		else
+			for (int i = 1; i < argc; i++) {
+				if (argv[i] == "-s" || argv[i] == "--silent")continue;
+				if (cmd_detector(argv[i]))continue;
+				try { show(Expression(argv[i])); }
+				catch (const char* err) {
+					cerr << endl << "[Error] " << err << endl << endl;
+				}
+			}
 		return 0;
 	}
 
